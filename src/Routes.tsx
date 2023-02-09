f279bf0704e3bd898ee98655bc157199c2cf52c9
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-02-09 18:56:32 -0300

    component header

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 import { HomePage } from "./pages/Home";
 import { CompleteOrderPage } from "./pages/Home/CompleteOrder";
 
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/completeOrder" element={<CompleteOrderPage />} />
+            <Route path="/" element={<DefaultLayout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/completeOrder" element={<CompleteOrderPage />} />
+            </Route>
         </Routes>
     )
 }
