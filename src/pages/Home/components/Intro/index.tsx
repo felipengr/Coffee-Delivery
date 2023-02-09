612b775696378cfe44494b9abfb72e8529110563
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-02-09 19:21:32 -0300

    errado

---
+++ src/pages/Home/components/Intro/index.tsx
@@ -0,0 +1,13 @@
+import { IntroContainer, IntroContent } from "./styles";
+
+export function Intro() {
+    return (
+        <IntroContainer>
+            <IntroContent className="container">
+                <div>
+                    
+                </div>
+            </IntroContent>
+        </IntroContainer>
+    )
+}
