5dd06795b3eed061ded9a9acfc4af6a896bac38b
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-02-10 19:23:25 -0300

    typography

--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -1,12 +1,16 @@
 import { IntroContainer, IntroContent } from "./styles";
+import introImg from '../../../../assets/intro-img.png'
 
 export function Intro() {
     return (
         <IntroContainer>
             <IntroContent className="container">
                 <div>
-                    
+                    <section>
+                        
+                    </section>
                 </div>
+                <img src={introImg} />
             </IntroContent>
         </IntroContainer>
     )
