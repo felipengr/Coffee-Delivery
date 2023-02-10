7ea959b31aba1555c0e95d148c34a9e8f29c6536
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-02-10 19:55:33 -0300

    Componente Intro

--- src/pages/Home/components/Intro/styles.ts
+++ src/pages/Home/components/Intro/styles.ts
@@ -1,6 +1,7 @@
 import styled  from 'styled-components';
 import IntroBackgroundImg from '../../../../assets/intro-background.png'
 import { rgba } from 'polished'
+import { TitleText } from '../../../../components/Typography';
 
 export const IntroContainer = styled.section`
     width: 100%;
@@ -14,6 +15,9 @@ export const IntroContainer = styled.section`
         )
     `};
     background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 export const IntroContent = styled.div`
@@ -21,4 +25,16 @@ export const IntroContent = styled.div`
     align-items: center;
     justify-content: space-between;
     gap: 3.5rem;
+`
+
+export const IntroTitle = styled(TitleText)`
+    margin-bottom: 1rem;
+`
+
+export const BenefitsContainer = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 1.25rem;
+    margin-top: 4.125rem;
 `
