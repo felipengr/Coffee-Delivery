d98d4f6e72fdee6a4ed60b3c1007f11325286be6
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-02-11 16:24:20 -0300

    componente de pagamento header e botões

--- src/pages/Home/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/Home/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,8 +1,9 @@
-import { MapPinLine } from "phosphor-react";
+import { MapPinLine, CurrencyDollar } from "phosphor-react";
 import { useTheme } from "styled-components";
 import { TitleText } from "../../../../../components/Typography";
 import { SectionTitle } from "../SectionTitle";
 import { AddressForm } from "./AddressForm";
+import { PaymentMethodOptions } from "./PaymentMethodOptions";
 import { CompleteOrderFormContainer, FormSectionContainer } from "./styles";
 
 export function CompleteOrderForm() {
@@ -20,6 +21,14 @@ export function CompleteOrderForm() {
                     icon={<MapPinLine color={colors["brand-yellow-dark"]} size={22} />}
                /> 
                <AddressForm />
+            </FormSectionContainer>
+            <FormSectionContainer>
+               <SectionTitle
+                    title="Pagamento"
+                    subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
+                    icon={<CurrencyDollar color={colors["brand-purple"]} size={22} />}
+               /> 
+               <PaymentMethodOptions />
             </FormSectionContainer>
         </CompleteOrderFormContainer>
     )
