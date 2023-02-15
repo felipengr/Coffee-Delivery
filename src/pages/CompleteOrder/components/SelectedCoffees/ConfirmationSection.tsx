e3d43dc4c5482c23b99991af317b26d6833fbf5c
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-02-14 21:09:02 -0300

    form provider adress

--- src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
+++ src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
@@ -40,7 +40,11 @@ export function ConfirmationSection() {
                     R$ {formattedCartTotal}
                 </RegularText>
             </div>
-            <Button text="Confirmar Pedido" disabled={cartQuantity <= 0} />
+            <Button
+                text="Confirmar Pedido"
+                disabled={cartQuantity <= 0}
+                type="submit"
+            />
         </ConfirmationSectionContainer>
     )
 }
--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -1,12 +1,44 @@
 import { CompleteOrderForm } from "./components/CompleteOrderForm";
 import { SelectedCoffees } from "./components/SelectedCoffees";
 import { CompleteOrderContainer } from "./styles";
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm, FormProvider } from "react-hook-form";
+
+const confirmOderFormValidationSchema = zod.object({
+    cep: zod.string().min(1, "Informe o CEP"),
+    street: zod.string().min(1, 'Informe a rua'),
+    number: zod.string().min(1, 'Informe número'),
+    complement: zod.string(),
+    district: zod.string().min(1, 'Informe o bairro'),
+    city: zod.string().min(1, 'Informe a cidade'),
+    uf: zod.string().min(1, 'Informe o estado')
+})
+
+export type OrderData = zod.infer<typeof confirmOderFormValidationSchema>
+
+type ConfirmOrderFormData = OrderData;
 
 export function CompleteOrderPage() {
+    const confirmOrderForm = useForm<ConfirmOrderFormData>({
+        resolver: zodResolver(confirmOderFormValidationSchema)
+    })
+
+    const { handleSubmit } = confirmOrderForm
+
+    function handleConfirmOrder(data: ConfirmOrderFormData) {
+        console.log(data)
+    }
+
     return(
-        <CompleteOrderContainer className="container">
-            <CompleteOrderForm />
-            <SelectedCoffees />
-        </CompleteOrderContainer>
+        <FormProvider {...confirmOrderForm}>
+            <CompleteOrderContainer
+                className="container"
+                onSubmit={handleSubmit(handleConfirmOrder)}
+            >
+                <CompleteOrderForm />
+                <SelectedCoffees />
+            </CompleteOrderContainer>
+        </FormProvider>
     )
 }
